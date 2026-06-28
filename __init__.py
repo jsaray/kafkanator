59b031b2b398f166c7f0bf97336781576929a4e3
jsaray <jsaray@gmail.com>
2026-06-28 14:35:39 +0200

    ortograph

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 from .fairness import simmilarity_fairness_hash
 from .fairness import fairness_metrics_table
-from .dataviz import simmilar_subjects_treatment_plot
+from .dataviz import similar_subjects_treatment_plot
